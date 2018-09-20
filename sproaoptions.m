1f1a7b2ae9fb65eae314da3f12d93bacf328691e
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2018-09-20 20:45:40 +0200

    Added initial Ik to spline ROA options.

--- sproaoptions.m
+++ sproaoptions.m
@@ -16,6 +16,7 @@ classdef sproaoptions < pwroaoptions
 %            iteration. Specifically, zi is a column vector of monomials 
 %            used to specify si(xi) in the Gram matrix form, 
 %            si(xi)=zi(xi)'*C*zi(xi). [default =  monomials(xi, 1:2) ]
+%       -Ik: initial vector of cell indizes.
 %
 %% About
 %
@@ -30,11 +31,13 @@ classdef sproaoptions < pwroaoptions
 %%
 
 properties
-    %x -- inherited from ROAOPTIONS
+    %x   -- inherited from ROAOPTIONS
     SP;
-    %zi -- inherited from PWROAOPTONS
-    %zV -- inherited from ROAOPTIONS
+    %zi  -- inherited from PWROAOPTONS
+    %zV  -- inherited from ROAOPTIONS
     %zVi -- inherited from PWROAOPTIONS
+    %Vi0 -- inherited from PWROAOPTIONS
+    Ik;
 end
 
 methods
@@ -42,6 +45,18 @@ methods
         opt@pwroaoptions(SP.f{1}, SP.f(2:end), SP.getH, x, varargin{:});
         
         opt.SP = SP;
+    end
+    
+    function opt = set.Ik(opt,value)
+        if ~isvector(value) || ~isinteger(value)
+            error('Ik must be vector of integers.');
+        elseif min(value) < 1
+            error('Ik must not contain non-positive values.');
+        elseif iscolumn(value)
+            opt.Ik = value';
+        else
+            opt.Ik = value;
+        end
     end
 end
 
