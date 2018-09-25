73427a4c69583eb97417e716e0a312864e0fdc87
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2018-09-25 12:30:57 +0200

    Bug-fix left-hand side multiplication.

--- @splinemodel/mtimes.m
+++ @splinemodel/mtimes.m
@@ -1,12 +1,24 @@
-function sp = mtimes(sp,b)
+function sp = mtimes(sp1,sp2)
 % Binary mtimes operator.
 %
 % See MTIMES.
 
-if ~isa(sp,'splinemodel')
-    sp = mtimes(b,sp);
-elseif ~isa(b,'splinemodel')
-    sp.f = cellfun(@(c) mtimes(c,b), sp.f, 'UniformOutput', false);
+if ~isa(sp1,'splinemodel')
+    sp = mltimes(sp2,sp1);
+elseif ~isa(sp2,'splinemodel')
+    sp = mrtimes(sp1,sp2);
 else
-    sp = combine(sp,b,@mtimes);
+    sp = combine(sp1,sp2,@mtimes);
+end
+
+end
+
+function sp = mrtimes(sp,b)
+%MRTIMES Right-hand side multiplication sp*b.
+    sp.f = cellfun(@(c) mtimes(c,b), sp.f, 'UniformOutput', false);
+end
+
+function sp = mltimes(sp,b)
+%MLTIMES Left-hand side multiplication b*sp
+    sp.f = cellfun(@(c) mtimes(b,c), sp.f, 'UniformOutput', false);
 end
