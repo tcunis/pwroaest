d67d4b9990f7714b18394c276ba323140e93034f
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2018-09-25 20:28:46 +0200

    Bug-fix concatenation of spline models.

--- @splinemodel/horzcat.m
+++ @splinemodel/horzcat.m
@@ -7,15 +7,27 @@ case 1, sp = sp1;
 
 case 2
     if ~isa(sp1,'splinemodel')
-        sp = sp2;
-        sp.f = cellfun(@(c) horzcat(sp1,c), sp2.f, 'UniformOutput', false);
+        sp = horzlcat(sp2,sp1);
     elseif ~isa(sp2,'splinemodel')
-        sp = sp1;
-        sp.f = cellfun(@(c) horzcat(c,sp2), sp2.f, 'UniformOutput', false);
+        sp = horzrcat(sp1,sp2);
     else
         sp = combine(sp1,sp2,@horzcat);
     end
 
 otherwise
     sp = horzcat(horzcat(sp1,sp2),horzcat(varargin{:}));
+end
+
+end
+
+function sp = horzlcat(sp,b)
+% Vertical concatenation from left, [b sp].
+
+    sp.f = cellfun(@(c) horzcat(b,c), sp.f, 'UniformOutput', false);
+end
+
+function sp = horzrcat(sp,b)
+% Vertical concatenation from right, [sp b].
+
+    sp.f = cellfun(@(c) horzcat(c,b), sp.f, 'UniformOutput', false);
 end
--- @splinemodel/vertcat.m
+++ @splinemodel/vertcat.m
@@ -7,15 +7,27 @@ case 1, sp = sp1;
 
 case 2
     if ~isa(sp1,'splinemodel')
-        sp = sp2;
-        sp.f = cellfun(@(c) vertcat(sp1,c), sp2.f, 'UniformOutput', false);
+        sp = vertacat(sp2,sp1);
     elseif ~isa(sp2,'splinemodel')
-        sp = sp1;
-        sp.f = cellfun(@(c) vertcat(c,sp2), sp2.f, 'UniformOutput', false);
+        sp = vertbcat(sp1,sp2);
     else
         sp = combine(sp1,sp2,@vertcat);
     end
 
 otherwise
     sp = vertcat(vertcat(sp1,sp2),vertcat(varargin{:}));
+end
+
+end
+
+function sp = vertacat(sp,b)
+% Vertical concatenation from above, [b; sp].
+
+    sp.f = cellfun(@(c) vertcat(b,c), sp.f, 'UniformOutput', false);
+end
+
+function sp = vertbcat(sp,b)
+% Vertical concatenation from below, [sp; b].
+
+    sp.f = cellfun(@(c) vertcat(c,b), sp.f, 'UniformOutput', false);
 end
