53f042fb1e90fb3117b89a9f16d75488a9fe52cf
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2019-04-02 09:15:22 +0200

    Alternative SOS control synthesis implementation with reachability objective.

---
+++ pwreach.m
@@ -0,0 +1,472 @@
+function [gbar,V,gamma,K,iter] = pwreach(f1,f2,phi,xbar,x,roaopts)
+% Estimates lower bound of piece-wise region of attraction.
+%
+%% Usage & description
+%
+%   [beta,V,gamma,iter] = pwroaest(f1, f2, phi, x, ropts)
+%   [beta,V,gamma,s1,s2,si,iter] = pwroaest(...)
+%
+% Estimates the lower bound of the region-of-attraction of the piecewise
+% polynomial vector field
+%
+%          / f1(x)      if phi(x) <= 0;
+%   xdot = |
+%          \ f2(x)      else;
+%
+% about the equilibrium point x = 0 with phi(0) < 0.
+%
+% The piecewise ROA estimation problem is formulated as:
+%   max beta
+%   subject to:
+%         V-L1 in SOS
+%       -( (V-gamma) + (beta-p)*s1 ) in SOS
+%       -( (grad(V)*f1 + L2) + (gamma-V)*s2' - phi*si' ) in SOS
+%       -( (grad(V)*f2 + L2) + (gamma-V)*s2" + phi*si" ) in SOS
+%
+% The problem above is solved by a V-s iteration algorithm based on the
+% work of Balas et al. (2009).
+%
+% Inputs:
+%       -f1:  first polynomial vector field
+%       -f2:  second polynomial vector field
+%       -phi: boundary condition (scalar field)
+%       -x:   state-space vector as PVAR
+%       -ropts: options for piece-wise ROA estimation; see PWROAOPTIONS.
+%
+% Outputs:
+%       -beta: maximum beta
+%       -V:  Lyapunov function corresponding to beta
+%       -s1: multiplier for beta-step 
+%       -s2: multipliers [s2'; s2"] for gamma-step (Lyapunov gradient)
+%       -si: multipliers [si'; si"] for gamma-step (boundary condition)
+%       -iter: structure with fields V, beta, gamma, s1, s2, and time;
+%              contains the results for each iteration.
+%
+%% About
+%
+% * Author:     Torbjoern Cunis
+% * Email:      <mailto:torbjoern.cunis@onera.fr>
+% * Created:    2018-05-22
+% * Changed:    2019-01-15
+%
+%% See also
+%
+% See ROAEST, PWROAOPTIONS, PCONTAIN, PWPCONTAIN
+%%
+
+% information from options
+% p  = roaopts.p;
+zV = roaopts.zVi;
+% z1 = roaopts.z1i;
+z2 = roaopts.z2i;
+zg = roaopts.zgi;
+zi = roaopts.zi;
+zK = roaopts.zK;
+L2 = roaopts.L2;
+L1 = roaopts.L1;
+Q  = roaopts.Q;
+c  = roaopts.c;
+
+NstepBis = roaopts.NstepBis;
+sopts = roaopts.sosopts;
+gopts = roaopts.gsosopts;
+gopts.minobj = 0;
+gammamax = roaopts.gammamax;
+% betamax = roaopts.betamax;
+Vin = roaopts.Vi0;
+Kin = roaopts.Kin;
+
+u  = roaopts.u;
+
+display = roaopts.display;
+debug   = roaopts.debug;
+log = roaopts.log;
+logpath = roaopts.logpath;
+
+if ~strcmp(log,'none') && ~exist(logpath{1},'file')
+    [success,info] = mkdir(logpath{1});
+    if success && ~isempty(info)
+        warning(info)
+    elseif ~success
+        error(info)
+    end
+elseif exist(logpath{1},'file')
+    delete([logpath{1} '/*.mat']);
+end
+
+% Vdeg = zV.maxdeg;
+Nsteps = NstepBis;
+
+% initialize storage
+c0 = cell(Nsteps,1);
+iter= struct('V',c0,'K',c0,'gbar',c0,'gamma',c0,'s',c0,'si',c0,'sg',c0,'sj',c0,'time',c0,'aux',c0);
+
+% boundary condition at origin
+phi0 = double(subs(phi, x, zeros(size(x))));
+
+% boundary condition at target
+phi1 = double(subs(phi, x, xbar));
+
+% Lyapunov functions
+V = cell(size(zV));
+
+%% Run V-s iteration
+fprintf('\n---------------Beginning piecewise V-s iteration\n');
+biscount = 0;
+for i1=1:NstepBis
+    biscount = biscount+1;
+
+    tic;
+
+    if i1==1
+        if ~isempty(Kin)
+            K = Kin;
+        else
+            K = zeros(size(u));
+        end
+    elseif isempty(zK)
+        % nothing to do
+        
+    elseif gpre <= gmin
+        % local K-V-s problem
+        K = roaKstep(f1,c,[],x,u,zK,V{1},[],g,[],s,sg,L1,L2,sopts);
+        if isempty(K)
+            if strcmp(display,'on')
+                fprintf('local K-step infeasible at iteration = %d\n',i1);
+            end
+            break;
+        end
+    else
+        K = pwroaKstep(f1,f2,phi,c,[],x,u,zK,V,[],g,[],s,si,sg,sj,L1,L2,roaopts);
+        if isempty(K)
+            if strcmp(display,'on')
+                fprintf('common K-step infeasible at iteration = %d\n',i1);
+            end
+            break;
+        end
+    end        
+    
+    % system & constraints under control
+    if ~isempty(u)
+        fK1 = subs(f1,u,K);
+        fK2 = subs(f2,u,K);
+        cK  = polynomial(subs(c,u,K));
+    else
+        fK1 = f1;
+        fK2 = f2;
+        cK  = c;
+    end
+    
+    %======================================================================
+    % Find V step:
+    % Hold s1, s2, si, b, g fixed and solve the Gamma Step and Beta Step
+    % for Appropiate Lyapunov function with an additional constraint
+    % V-L1 in SOS
+    %======================================================================
+    if i1==1
+        if ~isempty(Vin)
+            V = Vin;
+        elseif phi0 < 0
+            % Construct Lyap function from linearization of LHS function
+            V{1}=linstab(fK1,x,Q);
+            if length(V) > 1
+                V{2} = V{1};
+            end
+        else
+            [V{:}]=pwlinstab(fK1,fK2,phi,x);
+        end
+        
+    elseif gpre <= gmin
+        % local V-s problem
+        [V{1},~] = reachvstep(fK1,cK,xbar,x,zV{1},gbar,g,[],s,sg,L1,L2,sopts);
+        if isempty(V{1})
+            if strcmp(display,'on')
+                fprintf('local V-step infeasible at iteration = %d\n',i1);
+            end
+            break;
+        elseif length(V) > 1
+            V{2} = V{1};
+        end
+    else
+        [V{:}] = pwreachvstep(fK1,fK2,phi,cK,xbar,x,zV,gbar,g,[],s,si,sg,sj,L1,L2,roaopts);
+        if isempty(V{1})
+            if strcmp(display,'on') && length(V) == 1
+                fprintf('common V-step infeasible at iteration = %d\n',i1);
+            elseif strcmp(display,'on')
+                fprintf('multiple V-step infeasible at iteration = %d\n',i1);
+            end
+            break;            
+        end
+    end
+    
+    if phi0 < 0
+        %======================================================================
+        % Pre Gamma Step: Solve the problem
+        % {x:V(x) <= gamma} is contained in {x:grad(V)*f1 < 0}
+        %======================================================================
+        gopts.maxobj = gammamax;
+        [gbnds,s] = pcontain(jacobian(V{1},x)*fK1+L2,V{1},z2{1},gopts);
+        if isempty(gbnds)
+            if strcmp(display,'on')
+                fprintf('pre gamma step infeasible at iteration = %d\n',i1);
+            end
+            break;
+        end
+        gpre = gbnds(1);
+        
+        %======================================================================
+        % Min Gamma Step: Solve the problem
+        % {x:V(x) <= gamma} is contained in {x:phi(x)<=0}
+        %======================================================================
+        gopts.maxobj = gammamax;
+        [gbnds,~] = pcontain(phi,V{1},[],gopts);
+        if isempty(gbnds)
+            if strcmp(display,'on')
+                fprintf('min gamma step infeasible at iteration = %d\n',i1);
+            end
+            break;
+        end
+        gmin = gbnds(1);
+        
+        if strcmp(debug,'on')
+            fprintf('debug: gpre = %4.6f \t gmin = %4.6f\n', gpre, gmin);
+        end
+
+    else
+        % origin at boundary
+        % no local problem possible
+        gpre = [];
+        gmin = [];
+    end
+    
+    if gpre <= gmin
+        % estimated region of attraction does not reach boundary
+        gstb = gpre;
+        si = polynomial;
+
+        if strcmp(display,'on')
+            fprintf('Local polynomial problem at iteration = %d\n',i1);
+        end
+    else
+        %==================================================================
+        % Gamma 1 Step: Solve the following problems
+        % {x:V(x) <= gamma1} intersects {x:phi(x) <= 0} is contained 
+        % in {x:grad(V)*f1 < 0}
+        % 
+        % max gamma1 subject to
+        %     -[grad(V)*f1 + (gamma1 - V)*s - phi*si] in SOS,  s,si in SOS
+        %==================================================================
+        gopts.maxobj = gammamax;
+        [gbnds,s1,si1] = pwpcontain(jacobian(V{1},x)*fK1+L2,  ...
+                                    V{1}, phi, z2{1}, zi{1}, gopts ...
+        );
+        if isempty(gbnds)
+            if strcmp(display,'on')
+                fprintf('gamma 1 step infeasible at iteration = %d\n',i1);
+            end
+            break;
+        end
+        g1 = gbnds(1);
+
+        %==================================================================
+        % Gamma 2 Step: Solve the following problems
+        % {x:V(x) <= gamma2} intersects {x:phi(x) > 0} is contained 
+        % in {x:grad(V)*f2 < 0}
+        %
+        % max gamma2 subject to
+        %     -[grad(V)*f2 + (gamma - V)*s + phi*si] in SOS,  s,si in SOS
+        %==================================================================
+        gopts.maxobj = gammamax;
+        [gbnds,s2,si2] = pwpcontain(jacobian(V{end},x)*fK2+L2,  ...
+                                    V{end}, -phi+L2, z2{end}, zi{end}, gopts ...
+        );
+        if isempty(gbnds)
+            if strcmp(display,'on')
+                fprintf('gamma 2 step infeasible at iteration = %d\n',i1);
+            end
+            break;
+        end
+        g2 = gbnds(1);
+
+        if strcmp(debug,'on')
+            fprintf('debug: g1 = %4.6f \t g2 = %4.6f\n', g1, g2);
+        end
+        
+        gstb  = min(g1,g2);
+    
+        s  = [s1  s2 ];
+        si = [si1 si2];
+    end
+
+    if gstb > .99*gammamax
+        if strcmp(display,'on')
+            fprintf('result of gamma step close to maximum (99%%) at iteration = %d\n',i1);
+        end
+        break;
+    end 
+    
+    if length(V) == 1 || any(gpre <= gmin)
+        %==================================================================
+        % Constraint Step: Solve the following problem
+        % {x: V(x) <= gamma} is contained in {x: c(x) <= 0}
+        %==================================================================
+        [gcons,sg] = pcontain(cK,V{1},zg{1},gopts);
+        if isempty(gcons)
+            if strcmp(display,'on')
+                fprintf('constraint step infeasible at iteration = %d\n',i1);
+            end
+            break;
+        end
+
+        gcon = gcons(1);
+        sj = polynomial;
+        
+        if strcmp(debug,'on')
+            fprintf('debug: gstb = %4.6f \t gcon = %4.6f\n', gstb, gcon);
+        end
+        
+        g = min([gstb, gcon]);
+        
+        %==================================================================
+        % Level set at target
+        % gbar = V(xbar)
+        %==================================================================
+        gbar = subs(V{1},x,xbar);
+
+    else
+        %==================================================================
+        % Constraint 1 Step: Solve the following problem
+        % {x: V1(x) <= gamma} intersects {x:phi(x) <= 0} is contained in 
+        % {x: c(x) <= 0}
+        %==================================================================
+        [gcons,sg1,sj1] = pwpcontain(cK,V{1},phi,zg{1},zi{1},gopts);
+        if isempty(gcons)
+            if strcmp(display,'on')
+                fprintf('constraint 1 step infeasible at iteration = %d\n',i1);
+            end
+            break;
+        end
+
+        gc1 = gcons(1);
+        
+        %==================================================================
+        % Constraint 2 Step: Solve the following problem
+        % {x: V2(x) <= gamma} intersects {x:phi(x) > 0} is contained in 
+        % {x: c(x) <= 0}
+        %==================================================================
+        [gcons,sg2,sj2] = pwpcontain(cK,V{2},-phi+L2,zg{end},zi{end},gopts);
+        if isempty(gcons)
+            if strcmp(display,'on')
+                fprintf('constraint 2 step infeasible at iteration = %d\n',i1);
+            end
+            break;
+        end
+        
+        gc2 = gcons(1);
+        
+        if strcmp(debug,'on')
+            fprintf('debug: gstb = %4.6f \t gc1 = %4.6f \t gc2 = %4.6f\n', gstb, gc1, gc2);
+        end
+        
+        gcon = min(gc1,gc2);
+        g = min([gstb, gcon]);
+        
+        sg = [sg1 sg2];
+        sj = [sj1 sj2];
+        
+        %======================================================================
+        % Level set 1 at target
+        % gbar <= V1(xbar)
+        %======================================================================
+        gb1 = subs(V{1},x,xbar);
+        
+        %======================================================================
+        % Level set 2 at target
+        % gbar <= V2(xbar)
+        %======================================================================
+        gb2 = subs(V{2},x,xbar);
+        
+        if strcmp(debug,'on')
+            fprintf('debug: gb1 = %4.6f \t gb2 = %4.6f\n', gb1, gb2);
+        end
+        
+        if phi1 <= 0
+            % target in LHS
+            gbar = gb1;
+        else
+            % target in RHS
+            gbar = gb2;
+        end
+    end
+    
+    if ~strcmp(roaopts.gammacheck, 'none') && gpre > gmin
+        %==============================================================
+        % Gamma Check Step: Solve the following problems
+        % {x:V(x) <= gamma} intersects {x:phi(x) <= 0} is contained 
+        % in {x:grad(V)*f1 < 0}
+        %
+        % and
+        %
+        % {x:V(x) <= gamma} intersects {x:phi(x) > 0} is contained 
+        % in {x:grad(V)*f2 < 0}
+        %
+        % with gamma = min(gamma1,gamma2)
+        %==============================================================
+        [s1,si1] = pwpcontain_check(jacobian(V{1},x)*fK1+L2,  ...
+                                    V{1}-g, phi, z2{1}, zi{1}, gopts ...
+        );
+        [s2,si2] = pwpcontain_check(jacobian(V{end},x)*fK2+L2,  ...
+                                    V{end}-g, -phi+L2, z2{end}, zi{end}, gopts ...
+        );
+
+        if isempty(s1) || isempty(s2)
+            if strcmp(display,'on')
+                fprintf('gamma check step infeasible at iteration = %d\n',i1);
+            end
+            if strcmp(roaopts.gammacheck, 'check')
+                break;
+            end
+        else    
+            s  = [s1  s2 ];
+            si = [si1 si2];
+        end
+    end
+    
+    % Print results and store iteration data
+    if strcmp(display,'on')
+        fprintf('iteration = %d  \t gbar = %4.6f \t gamma = %4.6f\n',i1,gbar,g);
+    end
+    iteration.V     = V;
+    iteration.K     = K;
+    iteration.gbar  = double(gbar);
+    iteration.gamma = g;
+%     iteration.s0    = s0;
+    iteration.s     = s;
+    iteration.si    = si;
+    iteration.sg    = sg;
+    iteration.sj    = sj;
+    iteration.aux   = struct('gstb',gstb,'gcon',gcon,'gpre',gpre,'gmin',gmin);
+    iteration.time  = toc;
+    iter(i1) = iteration;
+    if strcmp(log,'step')
+        save([logpath{:} sprintf('iter%d',i1)], '-struct', 'iteration');
+    end
+end
+if strcmp(display,'on')
+    fprintf('---------------Ending V-s iteration.\n');
+end
+    
+%% Outputs
+iter(biscount+1:end) = [];
+[~, idx] = min([iter.gbar +Inf]);     % handle empty beta value(s)
+result = iter(idx);
+gbar  = result.gbar;
+V     = result.V;
+K     = result.K;
+gamma = result.gamma;   % handle empty gamma value
+
+
+if any(strcmp(log,{'step' 'result'}))
+    save([logpath{:} 'result'], 'iter');
+    save([logpath{:} 'result'], '-struct', 'result', '-append');
+end
---
+++ pwreachvstep.m
@@ -0,0 +1,163 @@
+function varargout = pwreachvstep(f1,f2,phi,con,xbar,x,z,gbar,gamma,s0,s,si,sg,sj,L1,L2,roaopts)
+% Solves the V-s step of the piecewise ROA iteration.
+%
+%% Usage & description
+%
+%   [V,c] = pwroavstep(f1,f2,phi,p,x,z,beta,gamma,s0,s2,si)
+%   [...] = pwroavstep(...,L1,L2,opts)
+%
+% Solves for a common Lyapunov function V which satisfies the SOS
+% constraints while holding all other variables fixed.
+%
+% Inputs:
+%       -f1:  first polynomial vector field
+%       -f2:  second polynomial vector field
+%       -phi: boundary condition (scalar field)
+%       -con: constraint function (scalar polynomial)
+%       -p:   shape function (scalar field)
+%       -x:   state-space vector as PVAR
+%       -z:   Nz-by-1 column vector of monomials; specifies the Lyapunov
+%             function decision variable in the vector form V(x) = c'*z(x).
+%       -beta:  level set of shape function
+%       -gamma: level set of the Lyapunov function
+%       -s1:  multiplier for beta-step; in the multiple V-step, 
+%             multipliers [s1'; s2"].
+%       -s2:  multipliers [s2'; s2"] for gamma-step (Lyapunov gradient)
+%       -si:  multipliers [si'; si"] for gamma-step (boundary condition)
+%       -sg:  multiplier for constraint step; in the multiple V-step,
+%             multipliers [sg'; sg"] (Lyapunov gradient)
+%       -sj:  multipliers [sj'; sj"] for constraint step (boundary
+%             condition) in the multiple V-step.
+%       -L1:  epsilon 1 (double or scalar field); enforces strict positive 
+%             definiteness of the Lyapunov function.
+%       -L2:  epsilon 2 (double or scalar field); enforces strict negative
+%             definiteness of the Lyapunov gradients.
+%       -opts:  SOS options structre; see SOSOPTIONS.
+%
+% Outputs:
+%       -V: Lyapunov function satisfying the SOS constraints; will be empty
+%           if no feasible solution is found.
+%       -c: Nz-by-1 vector of coefficients of the returned Lyapunov
+%           function.
+%
+%% About
+%
+% * Author:     Torbjoern Cunis
+% * Email:      <mailto:torbjoern.cunis@onera.fr>
+% * Created:    2018-05-23
+% * Changed:    2019-01-21
+%
+%% See also
+%
+% See PWROAEST, ROAVSTEP
+%%
+
+opts = roaopts.sosopts;
+zi1  = roaopts.zi{1};
+zi2  = roaopts.zi{end};
+
+
+if length(z) == 1
+    % common Lyapunov function
+    varargout = cell(1,2);
+    
+    % Lyapunov decision variable
+    [V,c] = polydecvar('c',z{1});
+
+    %% Common V-s feasibility problem
+    sosconstr = cell(5,1);
+
+    % V-L1 in SOS
+    sosconstr{1} = V >= L1;
+
+    % V(xbar) <= gbar
+    Vbar = subs(V,x,xbar);
+    sosconstr{2} = Vbar <= gbar;
+
+    % {x: V(x) <= g} is contained in {x: grad(V)*f < 0}
+    gradV = jacobian(V,x);
+    % -( pa + (g-p2)*s - phi*si ) in SOS
+    sosconstr{3} = -(gradV*f1 + L2 + s(1)*(gamma-V) - si(1)*phi) >= 0;
+    % -( pb + (g-p2)*s + (phi-l)*si ) in SOS
+    sosconstr{4} = -(gradV*f2 + L2 + s(2)*(gamma-V) + si(2)*(phi-L2)) >= 0;
+    
+    % {x: V(x) <= g} is contained in {x: c(x) <= 0}
+    sosconstr{5} = -(con + sg*(gamma-V)) >= 0;
+
+    % solve problem
+    sosconstr = vertcat(sosconstr{:});
+    [info,dopt] = sosopt(sosconstr,x,opts);
+
+    % output
+    if info.feas
+        varargout{1}   = subs(V,dopt);
+        varargout{end} = subs(c,dopt);
+    end
+else
+    % multiple Lyapunov functions
+    varargout = cell(1,4);
+
+    % Lyapunov decision variables
+    [V1,c1] = polydecvar('c1',z{1});
+    [V2,c2] = polydecvar('c2',z{2});
+    
+    % continuity decision variables
+    ri = [ sosdecvar('ci1',zi1)
+           sosdecvar('ci2',zi2)
+           sosdecvar('ci3',zi1)
+           sosdecvar('ci4',zi2)
+    ];
+    
+    %% Multiple V-s feasibility problem
+    sosconstr = cell(14,1);
+    
+    % Vi-L1 in SOS
+    sosconstr{1} = V1 >= L1;
+    sosconstr{2} = V2 >= L1;
+    
+    if double(subs(phi,x,xbar)) <= 0
+        % V1(xbar) <= gbar
+        V1bar = subs(V1,x,xbar);
+        sosconstr{3} = V1bar <= gbar;
+    else
+        % V2(xbar) <= gbar
+        V2bar = subs(V2,x,xbar);
+        sosconstr{4} = V2bar <= gbar;
+    end
+    
+    % {x: V(x) <= g} is contained in {x: grad(V)*f < 0}
+    gradV1 = jacobian(V1,x);
+    gradV2 = jacobian(V2,x);
+    % -( pa + (g-p2)*s - phi*si ) in SOS
+    sosconstr{5} = -(gradV1*f1 + L2 + s(1)*(gamma-V1) - si(1)*phi) >= 0;
+    % -( pb + (g-p2)*s + (phi-l)*si ) in SOS
+    sosconstr{6} = -(gradV2*f2 + L2 + s(2)*(gamma-V2) + si(2)*(phi-L2)) >= 0;
+    
+    % {x: V1(x) <= g} intersects {x: phi(x) <= 0} is contained in {x: c(x) <= 0}
+    sosconstr{7} = -(con + sg(1)*(gamma - V1) - sj(:,1)*phi) >= 0;
+    % {x: V2(x) <= g} intersects {x: phi(x) > 0} is contained in {x: c(x) <= 0}
+    sosconstr{8} = -(con + sg(2)*(gamma - V2) + sj(:,2)*(phi-L2)) >= 0;
+    
+    % {x: phi(x) <= 0} intersects {x: phi(x) >= 0} is contained in {x: V1(x) <= V2(x)}
+    sosconstr{9} = -((V1-V2) + ri(1)*phi - ri(2)*phi) >= 0;
+    % {x: phi(x) <= 0} intersects {x: phi(x) >= 0} is contained in {x: V1(x) >= V2(x)}
+    sosconstr{10} = -((V2-V1) + ri(3)*phi - ri(4)*phi) >= 0;
+    
+    sosconstr{11} = ri(1) >= 0;
+    sosconstr{12} = ri(2) >= 0;
+    sosconstr{13} = ri(3) >= 0;
+    sosconstr{14} = ri(4) >= 0;
+    
+    % solve problem
+    sosconstr = vertcat(sosconstr{:});
+    [info,dopt] = sosopt(sosconstr,x,opts);
+
+    % output
+    if info.feas
+        varargout{1} = subs(V1,dopt);
+        varargout{2} = subs(V2,dopt);
+        varargout{3} = subs(c1,dopt);
+        varargout{4} = subs(c2,dopt);
+    end
+
+end    
